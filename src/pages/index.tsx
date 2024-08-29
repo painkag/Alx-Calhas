2fdd20788afe1e5b4976a01d778738a7d5f8a03a
Victor Hugo <victorcamargo9901@gmail.com>
2024-08-29 18:15:21 -0300

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Headerr from "./Header/header";
@@ -8,24 +7,32 @@ import QuemSomos from "./Quem somos/quemSomos";
 import Orcamento from "./Orcamento/orcamento";
 import Produtos from "./NossosProdutos/produtos";
 import Footer from "./Footer/footer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-    <Head>
+      <Head>
         <title>Alx Calhas</title>
-        <meta name="description" content="Oferecemos instalação e manutenção de calhas e rufos, garantindo proteção e durabilidade para sua casa" />
+        <meta
+          name="description"
+          content="Oferecemos instalação e manutenção de calhas e rufos, garantindo proteção e durabilidade para sua casa"
+        />
+        <meta
+          name="google-site-verification"
+          content="6iM8cmC3G-WHJ7HNFKbMnYkqTJx1DPfNMY0KrAG3riQ"
+        />
         <link rel="icon" href="/Logo/Logo.png" />
       </Head>
-    <div className={styles.all}>
-      <Headerr/>
-      <ImagensSection/>
-      <QuemSomos/>
-      <Produtos/>     
-      <Orcamento/>
-      <Footer/>
-    </div>
+      <div className={styles.all}>
+        <Headerr />
+        <ImagensSection />
+        <QuemSomos />
+        <Produtos />
+        <Orcamento />
+        <Footer />
+      </div>
     </>
   );
 }
